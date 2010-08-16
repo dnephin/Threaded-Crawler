34f25335d3e34ce8d4d6f63177fd10a6e13a6145
Pontiffx <daniel@daniel-desktop.(none)>
2010-08-16 16:35:18 -0400

    Added some debug, fixed bugs Now running again.

---
+++ conf/hitw_cfg.py
@@ -0,0 +1,39 @@
+"""
+Configuration for hitw.
+
+
+"""
+
+
+CRAWLER_CONFIG = {
+	'number_threads': 100
+}
+
+
+AGENT_CONFIG = {
+
+	'FileStoreAgent': {
+		'base_path': 	"",
+		'on_duplicate_file': 'BACKUP',
+	}
+
+
+	'HttpAgent' = {
+		'http_timeout': 30,	
+	
+	}
+
+
+
+ROUTE = [
+	FollowA(url = 'http://doubleviking.com/hotties', 
+			regex = '.html', chain = [
+		FollowIMG(regex = '\.jpg', chain = [
+			SaveToFilesystemConditional()
+		])
+	],
+	FollowIMG(url = 'http://doubleviking.com/hotties',
+			regex = '\.jpg', chain = [
+		SavetoFileSystemConditional()
+	])
+]
---
+++ lib/commands/jobs.py
@@ -0,0 +1,30 @@
+"""
+ Commands specific to saveing Job Postings.
+"""
+
+import logging
+
+from commands.base import HttpFetchCommand, Command
+
+
+log = logging.getLogger("Command")
+
+
+
+
+class StoreToJobDatabase(HttpFetchCommand):
+	" Use the database agent to save a job posting to the database. "
+	
+
+	def __init__(self, chain=None, meta=[]):
+		self.meta = meta
+		Command.__init__(self, chain)
+
+	# TODO: save to database using DBAgent
+	def execute(self, work_unit):
+		" Fetch the url to save, and store it. "
+		resp = self.fetch(work_unit.url)
+
+		print "Save %s" % (work_unit.url)
+#		if resp.success():
+#			self.save(resp.content, work_unit.meta_data)
