61f0ec0a449cb8e7866f47b1924870e4ae59b795
Pontiffx <daniel@daniel-desktop.(none)>
2010-08-19 23:05:55 -0400

    Finished threads.py unit tests

--- test/test_threads.py
+++ test/test_threads.py
@@ -12,33 +12,42 @@ import time
 
 class SimpleCommand(Command):
 
-	def __init__(self):
+	def __init__(self, url=None):
+		self.url = url
 		Command.__init__(self, [])
 
 	def execute(self, work_unit):
 		work_unit.url = "NOW"
+
+class CounterObject(object):
+
+	counter = 0
 
 class ManyCommand(SimpleCommand):
 
 	def execute(self, work_unit):
 		if work_unit.url > 5:
 			return None
-		print work_unit.url
+		CounterObject.counter += 1
 		c = ManyCommand()
 		return [WorkUnit(c, url=work_unit.url+1), WorkUnit(c, url=work_unit.url+2)]
+
+class ExceptionCommand(SimpleCommand):
+
+	def execute(self, work_unit):
+		raise ValueError("lalala")
 
 
 class TestProcessingThread(unittest.TestCase):
 
 	def setUp(self):
 		self.q = Queue()
-		self.s = Semaphore(1)
+		self.s = Semaphore(5)
 		self.t = ProcessingThread(self.q, self.s)
 
 	def tearDown(self):
 		self.q.put(WorkUnit(shutdown=True))
 		self.t.join()
-		self.q.join()
 
 
 	def test_start_stop_working(self):
@@ -46,30 +55,39 @@ class TestProcessingThread(unittest.TestCase):
 		Test that start_working and stop_working properly alter the semaphore.
 		"""
 		self.t.start_working()
-		self.assertEquals(self.s._Semaphore__value, 0)
+		self.assertEquals(self.s._Semaphore__value, 4)
+		w = WorkUnit(SimpleCommand())
+		self.q.put(w)
+		self.q.get()
 		self.t.stop_working()
-		self.assertEquals(self.s._Semaphore__value, 1)
+		self.assertEquals(self.s._Semaphore__value, 5)
 
 
 	def test_run(self):
 		" Test that the thread runs the execute of command. "
-#		w = WorkUnit(SimpleCommand())
-#		self.q.put(w)
-#		time.sleep(0.5)
-#		self.assertEquals(w.url, "NOW")
+		w = WorkUnit(SimpleCommand())
+		self.q.put(w)
+		time.sleep(0.5)
+		self.assertEquals(w.url, "NOW")
 		
 
 	def test_run_many(self):
 		" Test that the run adds returned work units back into the queue. "
-		# TODO: better way to test this
-		#w = WorkUnit(ManyCommand(url=1))
-		#time.sleep(3)
+		w = WorkUnit(ManyCommand(), url=1)
+		self.q.put(w)
+		time.sleep(1.5)
+		self.assertEquals(CounterObject.counter, 12)
 
 	def test_run_exception(self):
-		pass
+		" Test that exceptions in commands do not cause the thread to die. "
+		w = WorkUnit(ExceptionCommand())
+		self.assertEquals(self.q.put(w), None)
 
 	def test_shutdown(self):
-		pass
+		" Test that the thread shuts down when asked. "
+		self.q.put(WorkUnit(shutdown=True))
+		time.sleep(0.5)
+		self.assertFalse(self.t.is_alive())
 
 
 if __name__ == '__main__':
